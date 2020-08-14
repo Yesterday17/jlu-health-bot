a000fa769e25b376e59561c66938492f65baf59d
Yesterday17 <t@yesterday17.cn>
2020-08-14 19:27:44 +0800

    Fix yoru time.

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def rpt(name, type: int):
 
 schedule.every().day.at("07:20").do(rpt("早打卡", 0))
 schedule.every().day.at("11:05").do(rpt("午打卡", 1))
-schedule.every().day.at("18:08").do(rpt("晚打卡", 2))
+schedule.every().day.at("17:08").do(rpt("晚打卡", 2))
 schedule.every().day.at("21:10").do(rpt("晚点名", 2))
 
 
